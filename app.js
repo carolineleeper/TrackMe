5db6a64dd4ac8b11a96659819ca595755dd63959
carolineleeper <caroline.leeper@gmail.com>
2018-08-14 07:32:14 +1000

    ENHANCE: use jQuery instead to append data to table

--- app.js
+++ app.js
@@ -4,21 +4,30 @@ devices.push({ user: "Mary", name: "Mary's iPhone" });
 devices.push({ user: "Alex", name: "Alex's Surface Pro" });
 devices.push({ user: "Mary", name: "Mary's MacBook" });
 
-devices.forEach(function(device) 
-{
-    const table = document.querySelector('#device');
-    const row = document.createElement('tr');
+// devices.forEach(function(device) 
+// {
+//     const table = document.querySelector('#devices');
+//     const row = document.createElement('tr');
     
-    const user = document.createElement('td');
-    const userText = document.createTextNode(device.user);
-    user.appendChild(userText);
+//     const user = document.createElement('td');
+//     const userText = document.createTextNode(device.user);
+//     user.appendChild(userText);
 
-    const name = document.createElement('td');
-    const nameText = document.createTextNode(device.name);
-    name.appendChild(nameText);
+//     const name = document.createElement('td');
+//     const nameText = document.createTextNode(device.name);
+//     name.appendChild(nameText);
 
-    row.appendChild(user);
-    row.appendChild(name);
+//     row.appendChild(user);
+//     row.appendChild(name);
 
-    table.appendChild(row);
+//     table.appendChild(row);
+// });
+
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+        <tr>
+            <td>${device.user}</td>
+            <td>${device.name}</td>
+        </tr>`
+    );
 });
