5c6b04795b3d4a715b095f51d6d86680e00be991
carolineleeper <caroline.leeper@gmail.com>
2018-08-15 19:55:31 +1000

    FIX: fix file structure

--- web/public/app.js
+++ web/public/app.js
@@ -1,35 +1,8 @@
 $('#navbar').load('navbar.html');
+$('#footer').load('footer.html');
 
-// JavaScript
-// const devices = [];
-// devices.push({ user: "Mary", name: "Mary's iPhone" });
-// devices.push({ user: "Alex", name: "Alex's Surface Pro" });
-// devices.push({ user: "Mary", name: "Mary's MacBook" });
-
-//jQuery
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
-// JavaScript
-// devices.forEach(function(device) 
-// {
-//     const table = document.querySelector('#devices');
-//     const row = document.createElement('tr');
-    
-//     const user = document.createElement('td');
-//     const userText = document.createTextNode(device.user);
-//     user.appendChild(userText);
-
-//     const name = document.createElement('td');
-//     const nameText = document.createTextNode(device.name);
-//     name.appendChild(nameText);
-
-//     row.appendChild(user);
-//     row.appendChild(name);
-
-//     table.appendChild(row);
-// });
-
-// jQuery
 devices.forEach(function(device) 
 {
     $('#devices tbody').append(`
@@ -40,16 +13,6 @@ devices.forEach(function(device)
     );
 });
 
-// JavaScript
-// document.querySelector('#add-device').addEventListener('click', function() 
-// {
-//     const user = document.querySelector('#user').value;
-//     const name = document.querySelector('#name').value;
-//     device.push({ user: user, name: name });
-//     console.log(devices);
-// });
-
-// jQuery
 $('#add-device').on('click', function () 
 {
     const user = $('#user').val();
