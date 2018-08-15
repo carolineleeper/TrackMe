fc3015390d128e87883e9e352c5839ab244f6ed3
carolineleeper <caroline.leeper@gmail.com>
2018-08-15 21:50:46 +1000

    ENHANCE: add local storage for username and password

--- web/public/app.js
+++ web/public/app.js
@@ -27,12 +27,23 @@ $('#send-command').on('click', function() {
     console.log(`command is: ${command}`);
 });
 
+
+// registration
+const users = JSON.parse(localStorage.getItem('users')) || [];
+
 $('#registerButton').on('click', () => {
     const username = $('#username').val();
     const password = $('#password').val();
     const confirmPassword = $('#confirmPassword').val();
 
+    users.push({ username, password, confirmPassword });
+    localStorage.setItem('users', JSON.stringify(users));
+
     console.log(username);
     console.log(password);
     console.log(confirmPassword);
+});
+
+const exists = users.find((user) => {
+    return user.name === username;
 });
